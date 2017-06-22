7870128d5680bc6e4ba0fc0f732ff40c8be28e5e
Hope Ngerebara <hope.ngerebara@andela.com>
2017-06-22 13:42:12 +0100

    users can create documents properly

--- client/src/components/document/ManageDocumentPage.js
+++ client/src/components/document/ManageDocumentPage.js
@@ -1,6 +1,6 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as documentActions from './DocumentActions';
 import DocumentForm from './DocumentForm';
 // import {authorsFormattedForDropdown} from '../../selectors/selectors';
@@ -11,43 +11,54 @@ export class ManageDocumentPage extends Component {
     super(props, context);
 
     this.state = {
-      document: Object.assign({}, props.document),
+      document: {
+        id: this.props.document.id ? this.props.document.id : null,
+        documentName: this.props.document.id ? this.props.document.documentName : '',
+        description: this.props.document.id ? this.props.document.description : '',
+        access: this.props.document.id ? this.props.document.access : 'public',
+        content: this.props.document.id ? this.props.document.content : ''
+      },
       errors: {},
       saving: false
     };
-    console.log(props)
     this.updateDocumentState = this.updateDocumentState.bind(this);
     this.saveDocument = this.saveDocument.bind(this);
   }
 
+componentDidMount() {
+  const currentPath = location.pathname;
+  if (currentPath === '/documents/new') {
+    const document = this.state.document;
+    this.setState({ document });
+  }
+}
   componentWillReceiveProps(nextProps) {
     if (this.props.document.id != nextProps.document.id) {
-      this.setState({document: Object.assign({}, nextProps.document)});
+      this.setState({ document: Object.assign({}, nextProps.document) });
     }
   }
 
   updateDocumentState(event) {
     const field = event.target.name;
-    let document = this.state.document;
+    const document = this.state.document;
     document[field] = event.target.value;
-    return this.setState({document: document});
+    return this.setState({ document });
   }
 
 
   saveDocument(event) {
     event.preventDefault();
-    this.setState({saving: true});
-
+    this.setState({ saving: true });
     this.props.actions.createDocument(this.state.document)
       .then(() => this.redirect())
-      .catch(error => {
+      .catch((error) => {
         toastr.error(error);
-        this.setState({saving: false});
+        this.setState({ saving: false });
       });
   }
 
   redirect() {
-    this.setState({saving: false});
+    this.setState({ saving: false });
     toastr.success('Document saved');
     this.context.router.push('/documents');
   }
@@ -76,21 +87,21 @@ ManageDocumentPage.contextTypes = {
 
 function getDocumentById(documents, id) {
   const document = documents.filter(document => document.id == id);
-  if (document) return document[0]; 
+  if (document) return document[0];
   return null;
 }
 
 function mapStateToProps(state, ownProps) {
-  const documentId = ownProps.params.id; 
+  const documentId = ownProps.params.id;
 
-  let document = { id: '', documentName: '', access: ''};
+  let document = { id: '', documentName: '', description: '', access: '', content: '' };
 
   if (documentId && state.documents) {
     document = getDocumentById(state.documents, documentId);
   }
 
   return {
-    document: document,
+    document,
   };
 }
 

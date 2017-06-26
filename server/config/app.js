6a7bb8b22786712248e3c5da4e8725f6fb1852d9
Hope Ngerebara <hope.ngerebara@andela.com>
2017-06-26 13:44:27 +0100

    add Procfile

--- server/config/app.js
+++ server/config/app.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../../', '/dist')));
 
 // middleware for token authentication
 app.use(authMiddleware.initialize());

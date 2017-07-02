5f335a3f71aca2668c766db87e8de1f721b26e8a
Hope Ngerebara <hope.ngerebara@andela.com>
2017-07-02 15:47:20 +0100

    fix view modal

--- client/src/components/document/ViewDocument.jsx
+++ client/src/components/document/ViewDocument.jsx
@@ -1,35 +1,41 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
-const ViewDocument = ({ document, edit }) => {
+class ViewDocument extends Component {
+  
+  componentDidMount() {
+    $('.doc-modal').modal();
+  }
 
-  return (
-    <div id="modal1" className="doc-modal modal modal-fixed-footer">
-      <div className="modal-content">
-        <h4>{document.documentName}</h4>
-        <p>Description: {document.description}</p>
-        <hr />
-        <p>{document.content}</p>
+  render(){
+    const { edit, document } = this.props;
+    return (
+      <div id="modal1" className="doc-modal modal modal-fixed-footer">
+        <div className="modal-content">
+          <h4>{document.documentName}</h4>
+          <p>Description: {document.description}</p>
+          <hr />
+          <p>{document.content}</p>
+        </div>
+        <div className="modal-footer">
+          <a
+            href="#!"
+            className="modal-action modal-close waves-effect waves-green btn-flat"
+          >
+            Close
+          </a>
+          { edit &&
+            <Link 
+              to={`/editDocument/${document.id}`} 
+              className="modal-action waves-effect waves-light btn modal-close" 
+            > <i className="material-icons left">mode_edit</i>
+            Edit</Link>
+          }
+        </div>
       </div>
-      <div className="modal-footer">
-        <a
-          href="#!"
-          className="modal-action modal-close waves-effect waves-green btn-flat"
-        >
-          Close
-        </a>
-        { edit && 
-          <Link 
-            to={`/editDocument/${document.id}`} 
-            className="modal-action waves-effect waves-light btn modal-close" 
-          > <i className="material-icons left">mode_edit</i>
-          Edit</Link>
-        }
-      </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 ViewDocument.propTypes = {};
 
 export default ViewDocument;

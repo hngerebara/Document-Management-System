20f110c7b0236b76742bf7edfc131af878c9d2af
Hope Ngerebara <hope.ngerebara@andela.com>
2017-07-10 13:48:03 +0100

    remove roles route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,11 @@
 import documentRoute from './documents';
 import userRoute from './users';
 import searchRoute from './search';
-import roleRoute from './roles';
 
 const allRoutes = (router) => {
   userRoute(router);
   documentRoute(router);
   searchRoute(router);
-  roleRoute(router);
 };
 
 export default allRoutes;

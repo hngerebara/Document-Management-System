b2b8ae7933f15462ea6299dcd164e50738117b0b
Hope Ngerebara <hope.ngerebara@andela.com>
2017-07-17 14:11:04 +0100

    webpack for production

---
+++ webpack.config.prod.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+require('dotenv').config();
+
+const DIST_DIR = path.resolve(__dirname, 'public');
+const SRC_DIR = path.resolve(__dirname, 'client/src/');
+
+const globalconstiables = {
+  API_URL: JSON.stringify(process.env.API_URL)
+};
+
+
+const config = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i, 
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash].[ext]'
+        }
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 5000,
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin(globalconstiables)
+  ]
+};
+
+
+module.exports = config;
